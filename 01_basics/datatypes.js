24877c594a5b60cb2a877d1b6680486b701d40bb
AkshatMaheshwari89 <akshat_monu007@yahoo.com>
2025-03-30 21:26:30 +0000

    datatypes & variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS as newer version
+
+// alert(3 + 3) // we are using nojejs, not browser
+
+console.log(3 / 3)
+
+console.log("Akshat")
+
+let name = "Akshat"
+let age = 34
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bugint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 011
+let accountemail = "akshat@google.com"
+var accountpassword = "1234"
+var accountcity;
+
+// accountId = 7 // not allowed
+
+accountemail = "abc@yahoo.com"
+accountpassword = 551086
+
+console.table ([accountemail, accountpassword, accountcity])
